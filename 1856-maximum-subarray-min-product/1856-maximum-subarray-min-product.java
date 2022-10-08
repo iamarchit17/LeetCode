6b813f7ac70984c605bce89659bed68b5ecb9ad0
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2022-10-08 14:40:58 +0530

    Time: 107 ms (73.79%), Space: 116.8 MB (18.22%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.java
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int maxSumMinProduct(int[] nums) {
         int[] left = new int[nums.length];
@@ -31,4 +32,40 @@ class Solution {
         
         return (int)(max % 1000000007);
     }
+}
+*/
+
+class Solution {
+    public int maxSumMinProduct(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        Deque<Integer> s = new ArrayDeque<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            while(s.size() != 0 && nums[s.peek()] >= nums[i]) s.pop();
+            left[i] = (s.size() == 0) ? 0 : s.peek() + 1; 
+            s.push(i);
+        }
+        
+        s= new ArrayDeque<>();
+        for(int i = nums.length - 1; i >= 0; i--){
+            while(s.size() != 0 && nums[s.peek()] >= nums[i]) s.pop();
+            right[i] = (s.size() == 0) ? nums.length - 1 : s.peek() - 1; 
+            s.push(i);
+        }
+        
+        
+        long[] sum = new long[nums.length];
+        sum[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) sum[i] = sum[i-1] + nums[i];
+        
+        long max = 0;
+        for(int i = 0; i < nums.length; i++){
+            long subArraySum = sum[right[i]] - ((left[i] == 0) ? 0 : sum[left[i] - 1]);
+            long minProd = subArraySum * nums[i];
+            if(minProd > max) max = minProd; 
+        }
+        
+        return (int)(max % 1000000007);
+    }    
 }
