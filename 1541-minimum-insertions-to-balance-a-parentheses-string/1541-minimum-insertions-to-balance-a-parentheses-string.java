35586be8a67180070f44f90c1bb5b3957098f686
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2022-10-10 11:55:01 +0530

    Time: 11 ms (95.79%), Space: 42.4 MB (98.22%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int minInsertions(String s) {
         Deque<Character> st = new ArrayDeque<>();
@@ -22,5 +23,34 @@ class Solution {
         }
         
         return minIns + 2 * st.size();
+    }
+}
+*/
+
+
+class Solution {
+    public int minInsertions(String s) {
+        int open = 0;
+        int minIns = 0;
+        for(int i = 0; i < s.length(); i++){
+            char x = s.charAt(i);
+            if(x == '(') open++;
+            else{
+                if(open == 0){
+                    open++;
+                    i--;
+                    minIns++;
+                } else {
+                    if(i+1 < s.length() && s.charAt(i+1) == ')'){
+                        i++;
+                    } else {
+                        minIns++;
+                    }
+                    open--;
+                }
+            }
+        }
+        
+        return minIns + 2 * open;
     }
 }
