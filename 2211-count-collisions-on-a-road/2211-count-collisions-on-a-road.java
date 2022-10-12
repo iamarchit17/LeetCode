dc2bdf2a7320829b561e91cc67896afb7f928a0f
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2022-10-12 11:04:15 +0530

    Time: 31 ms (58.70%), Space: 52.6 MB (85.65%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -1,3 +1,6 @@
+/*
+//Time - O(N)
+//Space - O(N)
 class Solution {
     public int countCollisions(String directions) {
         Deque<Character> s = new ArrayDeque<>();
@@ -22,6 +25,35 @@ class Solution {
                 
                 count++;
                 s.push('S');
+            }       
+        }
+        
+        return count;
+    }
+}
+*/
+
+
+class Solution {
+    public int countCollisions(String directions) {
+        int countR = 0;
+        boolean flagS = false;
+        int count = 0;
+        
+        for(int i = 0; i < directions.length(); i++){
+            char x = directions.charAt(i);
+            if(x == 'R') countR++;
+            else if(x == 'S'){
+                count += countR;
+                countR = 0;
+                flagS = true;
+            } else{
+                if(!flagS && countR == 0) continue;
+
+                count += countR;
+                countR = 0;
+                count++;
+                flagS = true;
             }       
         }
         
