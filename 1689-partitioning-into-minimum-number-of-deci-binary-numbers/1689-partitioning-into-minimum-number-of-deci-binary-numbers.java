654bb1674c3c65962fa661001c441937a787df3f
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-03-30 16:17:38 +0530

    Time: 7 ms (65.83%), Space: 43.2 MB (35.19%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minPartitions(String n) {
+        int x = 0;
+        for(int i = 0; i < n.length(); i++) if(n.charAt(i) > x) x = n.charAt(i);
+            
+        return x - 48;
+    }
+}
