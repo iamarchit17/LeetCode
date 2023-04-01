845ecd1c2d714ac24835c609aea1e3f716951136
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-04-01 23:30:04 +0530

    Time: 21 ms (57.28%), Space: 43.6 MB (29.30%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -4,10 +4,19 @@ class Solution {
         int aMoves = 0;
         int bMoves = 0;
         
-        for(int i = 0; i < s.length() - 2; i++){
-            if(s.charAt(i) == s.charAt(i + 1) && s.charAt(i+1) == s.charAt(i+2)){
-                if(s.charAt(i) == 'A') aMoves++;
-                else bMoves++;
+        int a = 0;
+        int b = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'A') a++;
+            else b++;
+            
+            if(a == 3) aMoves++;
+            if(b == 3) bMoves++;
+            
+            if(i - 2 >= 0) {
+                if(s.charAt(i - 2) == 'A') a--;
+                else b--;
             }
         }
         
