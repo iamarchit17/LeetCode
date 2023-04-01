a40e3a50a9c065010ceda883c84389fd7837576b
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-04-01 19:11:35 +0530

    Time: 83 ms (18.60%), Space: 51.5 MB (5.81%) - LeetHub

---
+++ 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int addOps = 0;
+        int multOps = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            int temp = nums[i];
+            int[] arr = new int[2];
+            while(temp > 0){
+                if(temp % 2 == 1){
+                    arr[0]++;
+                    temp--;
+                } else{
+                    arr[1]++;
+                    temp /= 2;
+                }
+            }
+            
+            addOps += arr[0];
+            multOps = Math.max(multOps, arr[1]);
+        }
+        
+        return addOps + multOps;
+    }
+}
