bb4335d4b1a14d35b1bc907757cc86ba275bb876
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-04-03 01:47:35 +0530

    Time: 16 ms (84.79%), Space: 42.8 MB (34.10%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
@@ -1,10 +1,10 @@
+/*
 class Solution {
     public String getSmallestString(int n, int k) {
         StringBuilder sb = new StringBuilder();
-        
+
         for(int i = 0; i < n; i++){
             for(int j = 1; j <= 26; j++){
-                //System.out.println(i + " " + j);
                 if((j + ((n - 1 - i) * 26)) >= k){
                     k = k - j;
                     char c = (char) (j + 96);
@@ -13,7 +13,25 @@ class Solution {
                 }
             }
         }
-        
+
         return sb.toString();
+    }
+}
+*/
+
+class Solution {
+    public String getSmallestString(int n, int k) {
+        char[] str = new char[n];
+        for(int i = 0; i < n; i++) str[i] = 'a';
+        k -= n;
+        
+        for(int i = n - 1; i >= 0; i--){
+            int min = Math.min(k, 25);
+            k -= min;
+            str[i] += min;
+            if(k == 0) break;
+        }
+
+        return String.valueOf(str);
     }
 }
