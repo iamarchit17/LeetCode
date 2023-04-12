e89c30d93b78e57db75e0cbf672c5ad984638168
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-04-12 16:11:28 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (92.52%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -24,6 +24,7 @@ class Pair{
 }
 
 
+/*
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
@@ -38,6 +39,37 @@ class Solution {
                 Pair temp = dq.poll();
                 if(temp.node.left != null) dq.add(new Pair(temp.node.left, temp.index * 2));
                 if(temp.node.right != null) dq.add(new Pair(temp.node.right, temp.index * 2 + 1));
+                
+                if(i == count - 1){
+                    width = temp.index - width + 1;
+                    maxWidth = Math.max(maxWidth, width);
+                }
+            }
+        }
+        
+        return maxWidth;
+    }
+}
+*/
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        Deque<Pair> dq = new ArrayDeque<>();
+        dq.add(new Pair(root, 1));
+        int maxWidth = 0;
+        
+        while(dq.size() != 0){
+            int count = dq.size();
+            int width = dq.peek().index;
+            for(int i = 0; i < count; i++){
+                Pair temp = dq.poll();
+                //temp.index - width keeps the indexes starting from 1 in each level
+                //it is not happening in this question, but for a big enough tree,
+                //the indexes will overflow, that is why keeping them starting from 1,
+                //does not cause the overflow
+                if(temp.node.left != null) dq.add(new Pair(temp.node.left, (temp.index - width) * 2));
+                if(temp.node.right != null) dq.add(new Pair(temp.node.right, (temp.index - width) * 2 + 1));
                 
                 if(i == count - 1){
                     width = temp.index - width + 1;
