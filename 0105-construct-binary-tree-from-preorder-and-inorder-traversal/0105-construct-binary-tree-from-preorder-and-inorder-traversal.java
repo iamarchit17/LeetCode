aa20fdb2ed235ab07071ae28dd6d4090f433e5e5
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-04-13 17:31:07 +0530

    Time: 2 ms (98.49%), Space: 42 MB (79.57%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+/*
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int[] index = {0};
@@ -21,18 +22,38 @@ class Solution {
     
     public TreeNode buildTree(int[] preorder, int[] inorder, int low, int high, int[] index){
         if(low > high) return null;
-        if(low == high){
-            TreeNode temp = new TreeNode(preorder[index[0]]);
-            index[0]++;
-            return temp;
-        }
         
         TreeNode root = new TreeNode(preorder[index[0]]);
         int i = low;
-        for(; i <= high; i++) if(inorder[i] == preorder[index[0]]) break;
+        for(; i <= high; i++) if(inorder[i] == preorder[index[0]]) break;z
         index[0]++;
         root.left = buildTree(preorder, inorder, low, i - 1, index);
         root.right = buildTree(preorder, inorder, i + 1, high, index);
+        
+        return root;
+    }
+}
+*/
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int[] index = {0};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        
+        return buildTree(preorder, map, 0, preorder.length - 1, index);
+    }
+    
+    public TreeNode buildTree(int[] preorder, HashMap<Integer, Integer> map, int low, int high, int[] index){
+        if(low > high) return null;
+        if(low == high) return new TreeNode(preorder[index[0]++]);
+            
+        TreeNode root = new TreeNode(preorder[index[0]++]);
+        int x = map.get(root.val);
+        root.left = buildTree(preorder, map, low, x - 1, index);
+        root.right = buildTree(preorder, map, x + 1, high, index);
         
         return root;
     }
