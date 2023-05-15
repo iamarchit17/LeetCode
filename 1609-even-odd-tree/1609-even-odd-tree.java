2b693208dd3293aae49cb7979e5206660a8f64dd
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-05-16 00:36:38 +0530

    Time: 15 ms (38.42%), Space: 57.6 MB (84.98%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        int level = 0;
+        
+        dq.add(root);
+        int count = 1;
+        int prev = Integer.MIN_VALUE;
+        
+        while(dq.size() != 0){
+            count--;
+            TreeNode temp = dq.poll();
+            
+            if(level % 2 == 0 && (temp.val <= prev || temp.val % 2 == 0)) return false;
+            if(level % 2 == 1 && (temp.val >= prev || temp.val % 2 == 1)) return false;
+            
+            prev = temp.val;
+            if(temp.left != null) dq.add(temp.left);
+            if(temp.right != null) dq.add(temp.right);
+            
+            if(count == 0){
+                count = dq.size();
+                level++;
+                prev = (level % 2 == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+        }
+        
+        return true;
+    }
+}
