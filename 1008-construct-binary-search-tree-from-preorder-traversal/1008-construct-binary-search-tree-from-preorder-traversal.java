5e83785ecef562f183e0bc58b6dc155d5f14386f
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-05-15 17:54:56 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (46.30%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,33 +14,50 @@
  * }
  */
 
-//Inorder traversal method
+//Inorder traversal method O(N logN)
+// class Solution {
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//         int[] inorder = preorder.clone();
+//         Arrays.sort(inorder);
+        
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int i = 0; i < inorder.length; i++){
+//             map.put(inorder[i], i);
+//         }
+        
+//         int[] index = new int[1];
+        
+//         return bstFromPreorder(preorder, map, 0, inorder.length - 1, index);
+        
+//     }
+    
+//     public TreeNode bstFromPreorder(int[] preorder, HashMap<Integer, Integer> map, int low, int high, int[] index){
+//         if(low > high) return null;
+//         if(low == high) return new TreeNode(preorder[index[0]++]);
+        
+//         TreeNode root = new TreeNode(preorder[index[0]++]);
+//         int x = map.get(root.val);
+//         root.left = bstFromPreorder(preorder, map, low, x-1, index);
+//         root.right = bstFromPreorder(preorder, map, x+1, high, index);
+        
+//         return root;
+        
+//     }
+// }
+
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
-        int[] inorder = preorder.clone();
-        Arrays.sort(inorder);
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < inorder.length; i++){
-            map.put(inorder[i], i);
-        }
-        
         int[] index = new int[1];
-        
-        return bstFromPreorder(preorder, map, 0, inorder.length - 1, index);
-        
+        return bstFromPreorder(preorder, Integer.MAX_VALUE, index);
     }
     
-    public TreeNode bstFromPreorder(int[] preorder, HashMap<Integer, Integer> map, int low, int high, int[] index){
-        if(low > high) return null;
-        if(low == high) return new TreeNode(preorder[index[0]++]);
+    public TreeNode bstFromPreorder(int[] preorder, int upper, int[] index){
+        if(index[0] >= preorder.length || preorder[index[0]] >= upper) return null;
         
         TreeNode root = new TreeNode(preorder[index[0]++]);
-        int x = map.get(root.val);
-        root.left = bstFromPreorder(preorder, map, low, x-1, index);
-        root.right = bstFromPreorder(preorder, map, x+1, high, index);
+        root.left = bstFromPreorder(preorder, root.val, index);
+        root.right = bstFromPreorder(preorder, upper, index);
         
         return root;
-        
     }
 }
