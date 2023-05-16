c7c43fd33639543b1e83f3b48d0f752346a61357
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-05-17 00:09:16 +0530

    Time: 25 ms (49.83%), Space: 47.3 MB (55.18%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.java
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.java
@@ -13,11 +13,56 @@
  *     }
  * }
  */
-class FindElements {
-    TreeNode root;
+
+//1059 ms
+// constructor takes O(N) time
+//find function takes O(N) time
+// class FindElements {
+//     TreeNode root;
     
-    public FindElements(TreeNode base) {
-        this.root = base;
+//     public FindElements(TreeNode base) {
+//         this.root = base;
+                
+//         root.val = 0;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+
+//         while(q.size() != 0){
+//             TreeNode temp = q.poll();
+            
+//             if(temp.left != null){
+//                 temp.left.val = 2 * temp.val + 1;
+//                 q.add(temp.left);
+//             }
+            
+//             if(temp.right != null){
+//                 temp.right.val = 2 * temp.val + 2;
+//                 q.add(temp.right);
+//             }
+//         }
+//     }
+    
+//     public boolean find(int target) {
+//         TreeNode temp = this.root;
+//         return postorder(temp, target);
+//     }
+    
+//     public boolean postorder(TreeNode root, int target){
+//         if(root == null) return false;
+        
+//         boolean x = postorder(root.left, target);
+//         boolean y = postorder(root.right, target);
+        
+//         return x || y || (root.val == target);
+        
+//     }
+// }
+
+class FindElements {
+    HashSet<Integer> set;
+    
+    public FindElements(TreeNode root) {
+        this.set = new HashSet<>();
                 
         root.val = 0;
         Queue<TreeNode> q = new LinkedList<>();
@@ -25,7 +70,7 @@ class FindElements {
 
         while(q.size() != 0){
             TreeNode temp = q.poll();
-            
+            set.add(temp.val);
             if(temp.left != null){
                 temp.left.val = 2 * temp.val + 1;
                 q.add(temp.left);
@@ -39,19 +84,10 @@ class FindElements {
     }
     
     public boolean find(int target) {
-        TreeNode temp = this.root;
-        return postorder(temp, target);
+        return set.contains(target);
     }
     
-    public boolean postorder(TreeNode root, int target){
-        if(root == null) return false;
-        
-        boolean x = postorder(root.left, target);
-        boolean y = postorder(root.right, target);
-        
-        return x || y || (root.val == target);
-        
-    }
+    
 }
 
 /**
