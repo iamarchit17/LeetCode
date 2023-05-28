0e7b50a4fc6206cea2b74661e581dec9f65d3f88
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-05-29 00:00:08 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -31,32 +31,74 @@ class DriverClass {
 
 /*Complete the function below*/
 
+//DFS Solution
+// class Solution {
+//     // Function to detect cycle in a directed graph.
+//     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+//         boolean[] visited = new boolean[V];
+//         boolean[] path = new boolean[V];
+//         for(int i = 0; i < V; i++){
+//             if(!visited[i]){
+//                 if(dfs(i, adj, visited, path)) return true;
+//             }
+//         }
+        
+//         return false;
+//     }
+    
+//     public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, boolean[] path){
+//         visited[node] = true;
+//         path[node] = true;
+        
+//         for(int x : adj.get(node)){
+//             if(path[x]) return true;
+//             else if(!visited[x]){
+//                 if(dfs(x, adj, visited, path)) return true;
+//             }
+//         }
+        
+//         path[node] = false;
+//         return false;
+//     }
+// }
+
+
+//BFS Solution - Topological Sorting
 class Solution {
     // Function to detect cycle in a directed graph.
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean[] visited = new boolean[V];
-        boolean[] path = new boolean[V];
+        
+        int[] inDegree = new int[V];
         for(int i = 0; i < V; i++){
-            if(!visited[i]){
-                if(dfs(i, adj, visited, path)) return true;
+            for(int x : adj.get(i)){
+                inDegree[x]++;
             }
         }
         
-        return false;
-    }
-    
-    public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, boolean[] path){
-        visited[node] = true;
-        path[node] = true;
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> topoSort = new ArrayList<>();
         
-        for(int x : adj.get(node)){
-            if(path[x]) return true;
-            else if(!visited[x]){
-                if(dfs(x, adj, visited, path)) return true;
+        for(int i = 0; i < V; i++){
+            if(inDegree[i] == 0) {
+                dq.add(i);
             }
         }
         
-        path[node] = false;
-        return false;
+        while(dq.size() != 0){
+            int x = dq.pop();
+            topoSort.add(x);
+            for(int i : adj.get(x)){
+                inDegree[i]--;
+                if(inDegree[i] == 0) dq.add(i);
+                
+            }
+        }
+        
+        return topoSort.size() < V;
     }
 }
+
+
+
+
+
