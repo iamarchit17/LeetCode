6d08293dc869604850686b8f9726837b92195665
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-05-29 00:45:54 +0530

    Time: 6 ms (66.56%), Space: 45 MB (9.32%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,3 +1,48 @@
+//DFS Solution
+// class Solution {
+//     public int[] findOrder(int n, int[][] preqs) {
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+//         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+//         for(int i = 0; i < preqs.length; i++) adj.get(preqs[i][1]).add(preqs[i][0]);
+        
+//         boolean[] visited = new boolean[n];
+//         boolean[] path = new boolean[n];
+        
+//         Deque<Integer> dq = new ArrayDeque<>();
+        
+//         for(int i = 0; i < n; i++){
+//             if(!visited[i]) dfs(i, visited, path, dq, adj);
+//         }
+        
+//         if(dq.size() < n) return new int[0];
+        
+//         int[] order = new int[n];
+//         for(int i = 0; i < n; i++) order[i] = dq.pop();
+        
+//         return order;
+//     }
+    
+//     public boolean dfs(int node, boolean[] visited, boolean[] path, Deque<Integer> dq, ArrayList<ArrayList<Integer>> adj){
+        
+//         visited[node] = true;
+//         path[node] = true;
+        
+//         for(int x : adj.get(node)){
+//             if(!visited[x]){
+//                 if(dfs(x, visited, path, dq, adj)) return true;
+//             } else if(path[x]) return true;
+//         }
+        
+//         path[node] = false;
+//         dq.push(node);
+        
+//         return false;
+//     }
+// }
+
+//BFS Solution - Kahn's Algorithm
+
 class Solution {
     public int[] findOrder(int n, int[][] preqs) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -5,37 +50,33 @@ class Solution {
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
         for(int i = 0; i < preqs.length; i++) adj.get(preqs[i][1]).add(preqs[i][0]);
         
-        boolean[] visited = new boolean[n];
-        boolean[] path = new boolean[n];
+        int[] inDegree = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int x : adj.get(i)) inDegree[x]++;
+        }
         
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> dq = new ArrayDeque<>();    
+        int[] order = new int[n];
+        int index = 0;
         
         for(int i = 0; i < n; i++){
-            if(!visited[i]) dfs(i, visited, path, dq, adj);
+            if(inDegree[i] == 0) dq.add(i);
         }
         
-        if(dq.size() < n) return new int[0];
+        while(dq.size() != 0){
+            int node = dq.poll();
+            order[index++] = node;
+            
+            for(int x : adj.get(node)){
+                inDegree[x]--;
+                if(inDegree[x] == 0){
+                    dq.add(x);
+                }
+            }
+        }
         
-        int[] order = new int[n];
-        for(int i = 0; i < n; i++) order[i] = dq.pop();
-        
-        return order;
+        return index < n ? new int[0] : order;
     }
     
-    public boolean dfs(int node, boolean[] visited, boolean[] path, Deque<Integer> dq, ArrayList<ArrayList<Integer>> adj){
-        
-        visited[node] = true;
-        path[node] = true;
-        
-        for(int x : adj.get(node)){
-            if(!visited[x]){
-                if(dfs(x, visited, path, dq, adj)) return true;
-            } else if(path[x]) return true;
-        }
-        
-        path[node] = false;
-        dq.push(node);
-        
-        return false;
-    }
+    
 }
