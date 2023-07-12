6183d2e40cd4df8d05f318123dad9ad1c81d07ab
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-07-12 15:52:28 +0530

    Time: 5 ms (98.90%), Space: 55.2 MB (21.41%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,39 +1,73 @@
 
 //BFS Solution - Topological Sort on Reverse Graph
+// class Solution {
+//     public List<Integer> eventualSafeNodes(int[][] graph) {
+//         ArrayList<ArrayList<Integer>> revAdjList = new ArrayList<>();
+//         for(int i = 0; i < graph.length; i++) revAdjList.add(new ArrayList<>());
+        
+//         for(int i = 0; i < graph.length; i++){
+//             for(int j = 0; j < graph[i].length; j++){
+//                 revAdjList.get(graph[i][j]).add(i);
+//             }
+//         }
+        
+//         int[] inDegree = new int[graph.length];
+//         for(int i = 0; i < revAdjList.size(); i++){
+//             for(int x : revAdjList.get(i)) inDegree[x]++;
+//         }
+        
+//         Deque<Integer> dq = new ArrayDeque<>();
+//         List<Integer> safe = new ArrayList<>();
+        
+//         for(int i = 0; i < graph.length; i++){
+//             if(inDegree[i] == 0) dq.add(i);
+//         }
+        
+//         while(dq.size() != 0){
+//             int node = dq.poll();
+//             safe.add(node);
+            
+//             for(int x : revAdjList.get(node)){
+//                 inDegree[x]--;
+//                 if(inDegree[x] == 0) dq.add(x);
+//             }
+//         }
+        
+//         Collections.sort(safe);
+//         return safe;
+//     }
+// }
+
+
+//DFS Solution
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ArrayList<ArrayList<Integer>> revAdjList = new ArrayList<>();
-        for(int i = 0; i < graph.length; i++) revAdjList.add(new ArrayList<>());
+        boolean[] visited = new boolean[graph.length];
+        boolean[] path = new boolean[graph.length];
         
         for(int i = 0; i < graph.length; i++){
-            for(int j = 0; j < graph[i].length; j++){
-                revAdjList.get(graph[i][j]).add(i);
-            }
+            if(!visited[i]) dfs(i, graph, visited, path);
         }
         
-        int[] inDegree = new int[graph.length];
-        for(int i = 0; i < revAdjList.size(); i++){
-            for(int x : revAdjList.get(i)) inDegree[x]++;
-        }
-        
-        Deque<Integer> dq = new ArrayDeque<>();
-        List<Integer> safe = new ArrayList<>();
-        
+        List<Integer> safe = new ArrayList<Integer>();
         for(int i = 0; i < graph.length; i++){
-            if(inDegree[i] == 0) dq.add(i);
+            if(!path[i]) safe.add(i);
         }
         
-        while(dq.size() != 0){
-            int node = dq.poll();
-            safe.add(node);
-            
-            for(int x : revAdjList.get(node)){
-                inDegree[x]--;
-                if(inDegree[x] == 0) dq.add(x);
-            }
-        }
-        
-        Collections.sort(safe);
         return safe;
+    }
+    
+    public boolean dfs(int node, int[][] graph, boolean[] visited, boolean[] path){
+        visited[node] = true;
+        path[node] = true;
+        
+        for(int x : graph[node]){
+            if(!visited[x]){
+                if(dfs(x, graph, visited, path)) return true;
+            } else if(path[x]) return true;
+        }
+        
+        path[node] = false;
+        return false;
     }
 }
