c1b8b5d761472e929e5a17a3332f1d9dfa435115
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-07-20 20:11:20 +0530

    Time: 7 ms (33.79%), Space: 40.9 MB (64.23%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -25,34 +25,53 @@
 // }
 
 // Memoization (Top-Down DP)
+// class Solution {
+//     public boolean isInterleave(String s1, String s2, String s3) {
+//         if(s1.length() + s2.length() != s3.length()) return false;
+        
+//         int[][] dp = new int[s1.length()][s2.length()];
+//         for(int i = 0; i < s1.length(); i++) Arrays.fill(dp[i], -1);
+        
+//         return isInterleave(s1, s2, s3, 0, 0, dp) == 1;
+//     }
+    
+//     public int isInterleave(String s1, String s2, String s3, int idx1, int idx2, int[][] dp){
+//         if(idx1 == s1.length()) return (s2.substring(idx2).equals(s3.substring(idx1 + idx2))) ? 1 : 0;
+//         if(idx2 == s2.length()) return (s1.substring(idx1).equals(s3.substring(idx1 + idx2))) ? 1 : 0;
+//         if(idx1 + idx2 == s3.length()) return (idx1 == s1.length() && idx2 == s2.length()) ? 1 : 0;
+        
+//         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+//         int ans = 0;
+//         int idx3 = idx1 + idx2;
+//         if(s1.charAt(idx1) == s3.charAt(idx3)) ans = ans | isInterleave(s1, s2, s3, idx1 + 1, idx2, dp);
+//         if(s2.charAt(idx2) == s3.charAt(idx3)) ans = ans | isInterleave(s1, s2, s3, idx1, idx2 + 1, dp);
+        
+//         return dp[idx1][idx2] = ans;
+        
+//     }
+// }
+
+
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length() + s2.length() != s3.length()) return false;
         
-        int[][] dp = new int[s1.length()][s2.length()];
-        for(int i = 0; i < s1.length(); i++) Arrays.fill(dp[i], -1);
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
         
-        return isInterleave(s1, s2, s3, 0, 0, dp) == 1;
-    }
-    
-    public int isInterleave(String s1, String s2, String s3, int idx1, int idx2, int[][] dp){
-        if(idx1 == s1.length()) return (s2.substring(idx2).equals(s3.substring(idx1 + idx2))) ? 1 : 0;
-        if(idx2 == s2.length()) return (s1.substring(idx1).equals(s3.substring(idx1 + idx2))) ? 1 : 0;
-        if(idx1 + idx2 == s3.length()) return (idx1 == s1.length() && idx2 == s2.length()) ? 1 : 0;
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[i].length; j++){
+                int idx = i + j - 1;
+                if(i == 0 && j == 0) dp[i][j] = true;
+                else if(i == 0) dp[i][j] = dp[i][j-1] && (s2.charAt(j-1) == s3.charAt(idx));
+                else if(j == 0) dp[i][j] = dp[i-1][j] && (s1.charAt(i-1) == s3.charAt(idx));
+                else dp[i][j] = (s1.charAt(i-1) == s3.charAt(idx) && dp[i-1][j]) || (s2.charAt(j-1) == s3.charAt(idx) && dp[i][j-1]);
+            }
+        }
         
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        
-        int ans = 0;
-        int idx3 = idx1 + idx2;
-        if(s1.charAt(idx1) == s3.charAt(idx3)) ans = ans | isInterleave(s1, s2, s3, idx1 + 1, idx2, dp);
-        if(s2.charAt(idx2) == s3.charAt(idx3)) ans = ans | isInterleave(s1, s2, s3, idx1, idx2 + 1, dp);
-        
-        return dp[idx1][idx2] = ans;
-        
+        return dp[s1.length()][s2.length()];
     }
 }
-
-
 
 
 
