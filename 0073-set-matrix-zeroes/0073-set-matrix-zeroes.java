87b6ff3399bfc9bb45b4ebe6f67ef3939bb933cf
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-07-29 16:46:03 +0530

    Time: 1 ms (75.82%), Space: 44.4 MB (90.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,59 @@
+//O(m + n) space
+
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+        
+//         HashSet<Integer> rows = new HashSet<>();
+//         HashSet<Integer> cols = new HashSet<>();
+        
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(matrix[i][j] == 0){
+//                     rows.add(i);
+//                     cols.add(j);
+//                 }
+//             }
+//         }
+        
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(rows.contains(i) || cols.contains(j)) matrix[i][j] = 0;
+//             }
+//         }
+        
+//     }
+// }
+
+ 
+//O(1) space
+
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        
-        HashSet<Integer> rows = new HashSet<>();
-        HashSet<Integer> cols = new HashSet<>();
+        int col0 = 1;
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(matrix[i][j] == 0){
-                    rows.add(i);
-                    cols.add(j);
+                    matrix[i][0] = 0;
+                    if(j == 0) col0 = 0;
+                    else matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(rows.contains(i) || cols.contains(j)) matrix[i][j] = 0;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
+        }
+        
+        if(matrix[0][0] == 0) Arrays.fill(matrix[0], 0);
+        if(col0 == 0){
+            for(int i = 0; i < m; i++) matrix[i][0] = 0;
         }
         
     }
