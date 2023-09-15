cdbb5a9acf87045a184c2f4f9335f5c88e698a4b
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-09-15 19:18:32 +0530

    Time: 811 ms (10.06%), Space: 117.4 MB (5.63%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,3 +1,4 @@
+//Minimum Spanning Tree (Kruskal Algorithm)
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         List<List<Integer>> edges = new ArrayList<>();
@@ -57,3 +58,55 @@ class DisjointSet{
         }
     }
 }
+
+// //Prim's Algorithm (Time Limit Exceeded)
+// class Solution {
+//     public int minCostConnectPoints(int[][] points) {
+//         List<List<List<Integer>>> adj = new ArrayList<>();
+//         for(int i = 0; i < points.length; i++) adj.add(new ArrayList<>());
+//         for(int i = 0; i < points.length; i++){
+//             for(int j = i+1; j < points.length; j++){
+//                 int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+//                 List<Integer> l = new ArrayList<>();
+//                 l.add(j);
+//                 l.add(dist);
+//                 adj.get(i).add(new ArrayList<>(l));
+//                 l.set(0, i);
+//                 adj.get(j).add(new ArrayList<>(l));
+//             }
+//         }
+    
+//         PriorityQueue<Pair> pq = new PriorityQueue<>(points.length * points.length, (l1, l2) -> l1.dist - l2.dist);
+//         pq.add(new Pair(0, 0));
+        
+//         boolean[] visited = new boolean[points.length];
+//         int ans = 0;
+        
+//         while(pq.size() != 0){
+//             Pair p = pq.poll();
+//             int node = p.node;
+//             int d = p.dist;
+//             if(!visited[node]) ans += d;
+//             visited[node] = true;
+            
+//             for(List<Integer> nbr : adj.get(node)){
+//                 if(!visited[nbr.get(0)]){
+//                     pq.add(new Pair(nbr.get(0), nbr.get(1)));
+//                 }
+//             }
+//         }
+        
+//         return ans;
+//     }
+// }
+
+
+// class Pair{
+//     int node, dist;
+    
+//     public Pair(int node, int dist){
+//         this.node = node;
+//         this.dist = dist;
+//     }
+    
+// }
