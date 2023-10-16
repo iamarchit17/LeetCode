180352bf8630075a5ee95b49399e2644ad97b023
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-10-16 14:47:08 +0530

    Time: 4 ms (99.80%), Space: 54.5 MB (20.24%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,43 +1,27 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        int[] arr = new int[n];
-        int res = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0) arr[j] = matrix[i][j] == '0' ? 0 : 1;
-                else arr[j] = matrix[i][j] == '0' ? 0 : arr[j] + 1;
-            }
-            
-            res = Math.max(res, maximalSquare(arr));
-        }
-        
-        return res * res;
-    }
-    
-    public int maximalSquare(int[] arr){
-        int n = arr.length;
-        int[] width = new int[n];
-        
-        Deque<Integer> dq = new ArrayDeque<>();
-        for(int i = n - 1; i >= 0; i--){
-            while(dq.size() != 0 && arr[dq.peek()] >= arr[i]) dq.pop();
-            width[i] = dq.size() == 0 ? n : dq.peek();
-            dq.push(i);
-        }
-        
-        dq.clear();
-        
-        for(int i = 0; i < n; i++){
-            while(dq.size() != 0 && arr[dq.peek()] >= arr[i]) dq.pop();
-            width[i] = dq.size() == 0 ? width[i] : width[i] - dq.peek() - 1;
-            dq.push(i);
-        }
-        
         int max = 0;
-        for(int i = 0; i < n ; i++) max = Math.max(max, Math.min(width[i], arr[i]));
-        return max;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] mat = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                mat[i][j] = matrix[i][j] - 48;
+                if(mat[i][j] == 1) max = 1;
+            }
+        }
+        
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] == '1'){
+                    mat[i][j] = 1 + Math.min(Math.min(mat[i-1][j], mat[i][j-1]), mat[i-1][j-1]);
+                    if(mat[i][j] > max) max = mat[i][j];
+                }
+            }
+        }
+        
+        return max * max;
+        
     }
 }
