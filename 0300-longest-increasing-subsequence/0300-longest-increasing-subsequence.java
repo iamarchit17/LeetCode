705f425ece6f4807df3bf226cee502c8076c1d7e
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-10-24 22:19:30 +0530

    Time: 330 ms (12.79%), Space: 100.1 MB (5.17%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,43 +1,20 @@
-//Recursive
-// class Solution {
-//     public int lengthOfLIS(int[] nums) {
-//         return lengthOfLIS(nums, 0, -1);
-//     }
-    
-//     public int lengthOfLIS(int[] nums, int i, int prevIndex){
-//         if(i == nums.length) return 0;
-        
-//         int notTaken = lengthOfLIS(nums, i+1, prevIndex);
-//         int taken = -1;
-//         if(prevIndex == -1 || nums[i] > nums[prevIndex]){
-//             taken = 1 + lengthOfLIS(nums, i+1, i);
-//         }
-        
-//         return Math.max(taken, notTaken);
-//     }
-// }
-
-//DP Top-Down
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int[][] dp = new int[nums.length][nums.length];
         for(int i = 0; i < nums.length; i++) Arrays.fill(dp[i], -1);
-        return lengthOfLIS(nums, 0, -1, dp);
+        return lengthOfLIS(nums, -1, 0, dp);
     }
     
-    public int lengthOfLIS(int[] nums, int i, int prevIndex, int[][] dp){
-        if(i == nums.length) return 0;
+    public int lengthOfLIS(int[] nums, int prev, int idx, int[][] dp){
+        if(idx == nums.length) return 0;
         
-        if(prevIndex != -1 && dp[i][prevIndex] != -1) return dp[i][prevIndex];
+        if(prev != -1 && dp[prev][idx] != -1) return dp[prev][idx];
         
-        int notTaken = lengthOfLIS(nums, i+1, prevIndex, dp);
-        int taken = -1;
-        if(prevIndex == -1 || nums[i] > nums[prevIndex]){
-            taken = 1 + lengthOfLIS(nums, i+1, i, dp);
-        }
+        int notTaken = lengthOfLIS(nums, prev, idx + 1, dp);
+        int taken = 0;
+        if(prev == -1 || nums[prev] < nums[idx]) taken = 1 + lengthOfLIS(nums, idx, idx + 1, dp);
         
-        if(prevIndex > -1) dp[i][prevIndex] = Math.max(taken, notTaken);
+        if(prev != -1) dp[prev][idx] = Math.max(taken, notTaken);
         return Math.max(taken, notTaken);
     }
 }
