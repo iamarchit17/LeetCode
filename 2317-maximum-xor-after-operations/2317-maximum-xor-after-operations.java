d23654063afba943c951eda49220f6963bf49fca
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-11-28 17:52:34 +0530

    Time: 1 ms (100.00%), Space: 54.7 MB (35.32%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.java
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.java
@@ -1,15 +1,35 @@
+// class Solution {
+    
+//     // since you can take AND of nums[i] with (nums[i] xor x)
+//     //you cannot set a non-set bit in nums[i]
+//     //however you can unset a bit in nums[i] by the given operation
+//     //hence, if any bit is set in even number of numbers in nums, 
+//     //you can choose any one number and unset that bit
+//     //on xoring all the elements, it will contribute in maximizing xor of all numbers
+//     public int maximumXOR(int[] nums) {
+//         int res = 0;
+//         for(int i = 0; i < 31; i++){
+//             int count = 0;
+//             for(int j = 0; j < nums.length; j++){
+//                 if(((nums[j] >> i) & 1) == 1) count++;
+//             }
+            
+//             if(count != 0){
+//                 res = res | (1 << i);
+//             }
+//         }
+        
+//         return res;
+//     }
+// }
+
 class Solution {
+    
+    //the code written above is simply ORing all the elements
     public int maximumXOR(int[] nums) {
         int res = 0;
-        for(int i = 0; i < 31; i++){
-            int count = 0;
-            for(int j = 0; j < nums.length; j++){
-                if(((nums[j] >> i) & 1) == 1) count++;
-            }
-            
-            if(count != 0){
-                res = res | (1 << i);
-            }
+        for(int i = 0; i < nums.length; i++){
+           res = res | nums[i];
         }
         
         return res;
