dad80451091440b33bb7c05779662b9ada4fcd87
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-12-19 12:38:33 +0530

    Time: 110 ms (20.78%), Space: 48.6 MB (5.08%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        HashMap<String, HashSet<String>> adj = new HashMap<>();
+        
+        for(int i = 0; i < recipes.length; i++){
+            for(int j = 0; j < ingredients.get(i).size(); j++){
+                adj.putIfAbsent(ingredients.get(i).get(j), new HashSet<>());
+                adj.get(ingredients.get(i).get(j)).add(recipes[i]);
+            }
+        }
+        
+        HashMap<String, Integer> inDegree = new HashMap<>();
+        for(Map.Entry<String, HashSet<String>> entry : adj.entrySet()){
+            for(String s : entry.getValue()){
+                inDegree.put(s, inDegree.getOrDefault(s, 0) + 1);
+            }
+        }
+        
+        
+                    
+        HashSet<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        for(int i = 0; i < supplies.length; i++){
+            q.add(supplies[i]);
+            visited.add(supplies[i]);
+        }
+        
+        while(q.size() != 0){
+            String supp = q.poll();
+            if(adj.containsKey(supp)){
+                for(String nb : adj.get(supp)){
+                    inDegree.put(nb, inDegree.getOrDefault(nb, 1) - 1);
+                    if(inDegree.get(nb) == 0){
+                        q.add(nb);
+                        visited.add(nb);
+                    }
+                }
+            }
+        }
+        
+        
+        List<String> res = new ArrayList<>();
+        for(String s : recipes){
+            if(visited.contains(s)) res.add(s);
+        }
+        
+        
+        return res;
+    }
+    
+}
