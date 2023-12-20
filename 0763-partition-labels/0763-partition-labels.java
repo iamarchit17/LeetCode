547eed5887cdc6381c41b4684d0421fd8931eba2
Archit Agrawal <81585368+iamarchit17@users.noreply.github.com>
2023-12-20 22:44:25 +0530

    Time: 8 ms (48.96%), Space: 41.9 MB (11.55%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,38 +1,27 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] freq = new int[26];
+        
+        for(int i = 0; i < s.length(); i++) freq[s.charAt(i) - 97]++;
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        List<Integer> res = new ArrayList<>();
+        int prevIndex = -1;
+        
         for(int i = 0; i < s.length(); i++){
-            freq[s.charAt(i) - 97]++;
-        }
-            
-        HashMap<Character, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        
-        int i = 0;
-        while(i < s.length() && freq[s.charAt(i) - 97] == 1){
-            list.add(1);
-            i++;
-        }
-        
-        if(i < s.length()) map.put(s.charAt(i), freq[s.charAt(i) - 97] - 1);
-        int j = i;
-        i++;
-        
-        for(; i < s.length(); i++){
-            
-            if(map.containsKey(s.charAt(i))){
-                if(map.get(s.charAt(i)) == 1) map.remove(s.charAt(i));
-                else map.replace(s.charAt(i), map.get(s.charAt(i)) - 1);
-            } else {
-                if(freq[s.charAt(i) - 97] > 1) map.put(s.charAt(i), freq[s.charAt(i) - 97] - 1);
+            set.add(s.charAt(i));
+            freq[s.charAt(i) - 97]--;
+            if(freq[s.charAt(i) - 97] == 0){
+                set.remove(s.charAt(i));
             }
             
-            if(map.size() == 0){
-                list.add(i - j + 1);
-                j = i + 1;
+            if(set.size() == 0){
+                res.add(i - prevIndex);
+                prevIndex = i;
             }
         }
         
-        return list;
+        return res;
     }
 }
